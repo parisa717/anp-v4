48db7c8a45b47b011e3c04ef90f56e752fbd0309
parisa <parisaghorbany77@gmail.com>
2025-01-07 19:45:40 +0330

    add to main detail rout

--- apps/technical-admin/src/pages/areas/lazyComponents.ts
+++ apps/technical-admin/src/pages/areas/lazyComponents.ts
@@ -2,3 +2,4 @@ import { lazy } from 'react'
 
 export const AreasListPage = lazy(() => import('./areasList/ui/Page'))
 export const CreateAreaPage = lazy(() => import('./createArea/ui/Page'))
+export const AreaDetailPage = lazy(() => import('./areaDetail/ui/Page'))
--- apps/technical-admin/src/pages/areas/routes.tsx
+++ apps/technical-admin/src/pages/areas/routes.tsx
@@ -2,12 +2,14 @@ import { Route } from 'react-router'
 
 import { ROUTE_PATHS } from '@/shared/lib'
 
-import { AreasListPage, CreateAreaPage } from './lazyComponents'
+import { AreaDetailPage,AreasListPage, CreateAreaPage } from './lazyComponents'
 
 export const areasRoutes = (
   <>
     <Route path={ROUTE_PATHS.Areas.Root} element={<AreasListPage />}>
       <Route path={ROUTE_PATHS.Areas.Create.slice(ROUTE_PATHS.Areas.Root.length + 1)} element={<CreateAreaPage />} />
+      
     </Route>
+    <Route path={ROUTE_PATHS.Areas.Detail} element={<AreaDetailPage />} />
   </>
 )
