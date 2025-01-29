2198775f832bae432111aab6c196829b13e12e19
parisa <parisaghorbany77@gmail.com>
2025-01-29 12:23:36 +0330

    add assign location

--- apps/technical-admin/src/pages/areas/areaDetail/ui/Page.tsx
+++ apps/technical-admin/src/pages/areas/areaDetail/ui/Page.tsx
@@ -4,8 +4,10 @@ import { Button } from 'primereact/button'
 import { Outlet, useParams } from 'react-router'
 
 import { useGetAreaQuery } from '@/entities/area'
+import { useGetLocationsQuery } from '@/entities/location'
 
 import { useAreaColumns } from '../lib/useAreaColumns'
+import { useLocationsColumns } from '../lib/useLocationsColumns'
 
 const dataTablePt = {
   root: {
@@ -34,10 +36,17 @@ const AreaDetailPage = () => {
   const { t } = useTranslation()
 
   const areaColumns = useAreaColumns()
+  const locationColumns = useLocationsColumns()
 
   const { id = '' } = useParams<{ id: string }>()
 
   const { data: areaData, isLoading: isAreaDataLoading, isError: isErrorArea } = useGetAreaQuery({ id })
+  const { data: LocationData, isLoading: isLocationDataLoading } = useGetLocationsQuery(
+    { filter: { area: id } },
+    {
+      skip: !id,
+    },
+  )
 
   if (isErrorArea) {
     //TODO: Add proper error handling
@@ -50,14 +59,26 @@ const AreaDetailPage = () => {
     <main>
       <div className="flex justify-between items-center mb-4">
         <h1 className="text-headline">{translate('title')}</h1>
-
         <Button severity="secondary" outlined label={translate('EditAreaButton')} />
       </div>
+
       <DataTable
         columns={areaColumns}
         data={areaData ? [areaData] : []}
         loading={isAreaDataLoading}
         pt={{ ...dataTablePt, root: { ...dataTablePt.root, 'data-cy': 'area-table' } }}
+        emptyMessage={translate('table.empty')}
+      />
+      
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-4xl text-[#465161] ">{translate('AssignLocationTitle')}</h1>
+        <Button severity="secondary" outlined label={translate('AddLocationButton')} />
+      </div>
+
+      <DataTable
+        columns={locationColumns}
+        data={LocationData ?? []}
+        loading={isLocationDataLoading}
         emptyMessage={translate('table.empty')}
       />
 
