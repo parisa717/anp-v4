53af8c26711fa21f6009bc5ace4919af27f808d9
parisa <parisaghorbany77@gmail.com>
2025-01-30 14:43:53 +0330

    test

--- apps/technical-admin/src/pages/areas/areaDetail/ui/Page.component.cy.tsx
+++ apps/technical-admin/src/pages/areas/areaDetail/ui/Page.component.cy.tsx
@@ -1,15 +1,17 @@
-import { GET_AREA_OPERATION_DEFAULT_RESPONSE } from '@cypress-fixtures'
+import { GET_AREA_OPERATION_DEFAULT_RESPONSE ,GET_LOCATIONS_OPERATION_DEFAULT_RESPONSE} from '@cypress-fixtures'
 import { aliasQuery, errorResponse, hasOperationName, successResponse } from '@nexus-ui/utils'
 
 import { GqlAreaObjectTypeEntity } from '@/entities/area'
+import { LocationEntity } from '@/entities/location'
 
 import AreaDetailPage from './Page'
 
 const AREA_TABLE = '[data-cy="area-table"]'
 const CELL = '[data-pc-section="bodycell"]'
+const ROW = '[data-pc-section="bodyrow"]'
 
 const AREA_DATA = GET_AREA_OPERATION_DEFAULT_RESPONSE.getArea
-
+const LOCATIONS = GET_LOCATIONS_OPERATION_DEFAULT_RESPONSE.getLocations.locations
 const verifyTableRendering = (tableSelector: string, columnsData: string[]) => {
   cy.get(tableSelector)
     .find(CELL)
@@ -27,8 +29,16 @@ describe('AreaDetailsPage', () => {
       }
     })
 
+    cy.intercept('POST', import.meta.env.VITE_API_ENDPOINT, (req) => {
+      if (hasOperationName(req, 'GetLocations')) {
+        aliasQuery(req, 'GetLocations')
+        successResponse(req, GET_LOCATIONS_OPERATION_DEFAULT_RESPONSE)
+      }
+    })
+
     cy.mountWithProviders(<AreaDetailPage />)
     cy.wait('@gqlGetAreaQuery')
+    cy.wait('@qqlGetLocationsQuery')
   })
 
   it('renders the Area table correctly', () => {
@@ -56,7 +66,25 @@ describe('AreaDetailsPage', () => {
     cy.wait('@gqlGetAreaQuery')
     cy.contains('No area details found').should('be.visible')
   })
+ it('renders the table with locations', () => {
+    const getLocationFields = (location: LocationEntity) => [
+      location.area.id,
+      location.code,
+      location.name,
+      location.brands.map((b) => b.code).join(''),
+      location.address.postCode,
+      location.address.city,
+      location.address.address,
+      location.isActive ? 'active' : 'inactive',
+    ]
 
+    LOCATIONS?.forEach((location, locationIndex) => {
+      getLocationFields(location).forEach((field, fieldIndex) => {
+        console.log(field)
+        cy.get(ROW).eq(locationIndex).find(CELL).eq(fieldIndex).should('contain.text', field)
+      })
+    })
+  })
   it('does not render table rows when GQL query errors', () => {
     cy.intercept('POST', import.meta.env.VITE_API_ENDPOINT, (req) => {
       if (hasOperationName(req, 'GetArea')) {
